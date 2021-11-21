e786173b5529236cc9efe48b33e6f6150f1f9f08
kath <katya.permyakova.2004@mail.ru>
2021-11-21 13:49:33 +0300

    frst

---
+++ main.py
@@ -0,0 +1,46 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtCore import QPoint
+from PyQt5.QtWidgets import QWidget, QApplication
+from random import randrange
+
+
+class Wind(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('interface.ui', self)
+
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+        self.do_paint = False
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circl(qp)
+            qp.end()
+
+    def draw_circl(self, qp):
+        qp.setBrush(QColor('yellow'))
+        r = randrange(200)
+        qp.drawEllipse(QPoint(200, 180), r, r)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = Wind()
+    form.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
+
