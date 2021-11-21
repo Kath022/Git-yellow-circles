935462795ae7ab0d2dae06e3029a937a59d59682
kath <katya.permyakova.2004@mail.ru>
2021-11-21 14:10:05 +0300

    interface from python file

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ from PyQt5.QtCore import QPoint
 from PyQt5.QtWidgets import QWidget, QApplication
 from random import randrange
 
+from interfce import Ui_Form
 
-class Wind(QWidget):
+
+class Wind(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('interface.ui', self)
+        self.setupUi(self)
 
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
@@ -28,7 +30,7 @@ class Wind(QWidget):
             qp.end()
 
     def draw_circl(self, qp):
-        qp.setBrush(QColor('yellow'))
+        qp.setBrush(QColor(randrange(255), randrange(255), randrange(255)))
         r = randrange(200)
         qp.drawEllipse(QPoint(200, 180), r, r)
 
